2ddd4e8cbe81330d6d62dbdf731e3bc7cc02d179
zhouenxian <1052191886@qq.com>
2019-09-25 16:51:41 +0800

    init 项目

---
+++ Algorithms/0101.symmetric-tree/101.对称二叉树.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=golang
+ *
+ * [101] 对称二叉树
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package problem0101
+
+import (
+	"leetcode/kit"
+)
+
+type TreeNode = kit.TreeNode
+
+func isSymmetric(root *TreeNode) bool {
+
+	return false
+}
---
+++ Algorithms/0101.symmetric-tree/symmetric-tree_test.go
@@ -0,0 +1,43 @@
+package problem0101
+
+import (
+	"fmt"
+	"leetcode/kit"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Problem0101(t *testing.T) {
+	ast := assert.New(t)
+
+	// tcs is testcase slice
+	tcs := []struct {
+		pre, in []int
+		ans     bool
+	}{
+		{
+			[]int{},
+			[]int{},
+			true,
+		},
+
+		{
+			[]int{1, 2, 2},
+			[]int{2, 1, 2},
+			true,
+		},
+
+		{
+			[]int{1, 2, 3, 2, 3},
+			[]int{2, 3, 1, 2, 3},
+			false,
+		},
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, isSymmetric(kit.PreIn2Tree(tc.pre, tc.in)), "输入:%v", tc)
+	}
+}
