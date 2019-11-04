318d9a02c8dcc6b0718535a2e04e13f3bd75e01b
zhouenxian <1052191886@qq.com>
2019-11-04 17:21:44 +0800

    226完成

--- Algorithms/0226.invert-binary-tree/invert-binary-tree.go
+++ Algorithms/0226.invert-binary-tree/invert-binary-tree.go
@@ -7,5 +7,11 @@ import (
 type TreeNode = kit.TreeNode
 
 func invertTree(root *TreeNode) *TreeNode {
-
+	if root == nil {
+		return nil
+	}
+	cache:=invertTree(root.Right)
+	root.Right=invertTree(root.Left)
+	root.Left=cache
+	return root
 }
---
+++ Algorithms/0437.path-sum-iii/path-sum-iii.go
@@ -0,0 +1,11 @@
+package problem0437
+
+import (
+	"leetcode/kit"
+)
+
+type TreeNode = kit.TreeNode
+
+func pathSum(root *TreeNode, sum int) int {
+
+}
