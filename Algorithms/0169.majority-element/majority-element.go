d1897f27a143621ebefd3b4350de39780c89cca0
zhouenxian <1052191886@qq.com>
2020-04-30 16:51:10 +0800

    finish 169

--- Algorithms/0169.majority-element/majority-element.go
+++ Algorithms/0169.majority-element/majority-element.go
@@ -1,5 +1,34 @@
 package problem0169
 
-func majorityElement(nums []int) int {
+func majorityElement1(nums []int) int {
+	m := make(map[int]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		if _, ok := m[nums[i]]; !ok {
+			m[nums[i]] = 1
+		} else {
+			m[nums[i]] += 1
+		}
+	}
+	for i, v := range m {
+		if v > len(nums)/2 {
+			return i
+		}
+	}
+	return 0
+}
 
+func majorityElement(nums []int) int {
+	count := 1
+	current := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == current {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				current = nums[i+1]
+			}
+		}
+	}
+	return current
 }
